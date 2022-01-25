e9b716e34ab6a7e9cf9c53e845afc2b14c6abe0c
christine adriano <christine.g.adriano@accenture.com>
2022-01-25 21:08:17 +0800

    [MOVIESITE]: Added assets - fonts and icons

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,14 +1,11 @@
 import react, { useState } from "react";
 
 function Home() {
-    
-
     return (
         <>
         <h1> THIS IS IT PANSIT</h1>
         </>
     );
-
 };
 
 export default Home;
